052b10bf42857243e7848b8ff7ce979c32c6ad96
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2022-11-15 12:44:55 +0530

    Variables Objects

---
+++ src/test/java/basicsOfJava/ClassAndMethods.java
@@ -0,0 +1,30 @@
+package basicsOfJava;
+
+public class ClassAndMethods {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public void method1() {
+		
+	}
+	
+	public static void method2() {
+		
+	}
+	
+	static public void method3() {
+		
+	}
+	
+	int a = 10; // default access modifer. declared and initialized. Non static variable
+	static int a1; // a1 = 0
+	
+	public String str = "abc";
+	public static String str1; // str1 = null;
+	
+	
+	
+
+}
