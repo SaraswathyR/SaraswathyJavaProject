3a0da3f21a842bc8a01a02d4823b871198e35706
SaraswathyR <117795429+SaraswathyR@users.noreply.github.com>
2022-12-08 21:22:12 +0530

    Greatest of 2 and 3 eg

---
+++ src/main/java/examples/GreatestOfTwo.java
@@ -0,0 +1,29 @@
+package examples;
+
+
+public class GreatestOfTwo {
+
+	public static void main(String[] args) {
+		
+		
+		
+        
+		int a = 201;
+		int b = 100;
+		
+
+		if(a<b)
+		{
+			System.out.println(b + "is greater value");
+			
+		}
+		else
+			System.out.println(a + "is greater value");
+		
+			
+	
+		
+
+	}
+
+}
