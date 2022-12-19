0119594ff79aeb8717bb3e9db530d3e0c831f094
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2022-12-20 00:48:05 +0530

    collectionPractice main src

---
+++ src/main/java/collectionsPractice/ArrayListPractice.java
@@ -0,0 +1,12 @@
+package collectionsPractice;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<String>();
+	}
+
+}
