f4f09f9ef1db8776a4f8fcd6b416d3662615df19
SaraswathyR <117795429+SaraswathyR@users.noreply.github.com>
2022-12-31 11:02:23 +0530

    Practice prgms

---
+++ src/main/java/interviewPrograms/OccuranceOfSpecificChar.java
@@ -0,0 +1,22 @@
+package interviewPrograms;
+
+public class OccuranceOfSpecificChar {
+	
+	public static int occurance(String str, char c) {
+		int temp=0;
+		for(int i=0; i<str.length();i++) {
+			if(str.charAt(i)==c)
+				temp++;
+		}
+		return temp;
+	}
+
+	public static void main(String[] args) {
+		String str= "Saraswathy";
+		char c='a';
+		System.out.println(occurance(str,c));
+		
+
+	}
+
+}
