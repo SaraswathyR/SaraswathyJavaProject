50f8e46320077a9065381f48c9d393e5a0c4d405
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2023-02-13 23:14:24 +0530

    Actions class revisit

--- src/test/java/seleniumBasics/ActionClassPractice.java
+++ src/test/java/seleniumBasics/ActionClassPractice.java
@@ -10,10 +10,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionClassPractice {
-	
+
 	static WebDriver driver;
 
 	public static void main(String[] args) {
+		ActionClassPractice ACP = new ActionClassPractice();
+		ACP.launchBrowser("chrome");
+		ACP.launchApplication("Crocs", 30);
+		WebElement shopByStyle = driver.findElement(By.xpath("//h3[text() = 'Shop by Style']"));
+		ACP.scrollToAnElementUsingActions(shopByStyle);
+	}
+
+	public void test() {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\LENOVO\\git\\SaraswathyJavaProject\\drivers\\chromedriver_109.exe");
 		driver = new ChromeDriver();
@@ -26,26 +34,44 @@ public class ActionClassPractice {
 
 		Actions actions = new Actions(driver);
 //		actions.moveToElement(womenTab).build().perform();
-		
-	
-//		actions.contextClick(womenTab).build().perform();
-		
-//		actions.contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
-		
-		actions.moveToElement(womenTab).contextClick().pause(5000)
-		.keyDown(Keys.ARROW_DOWN).keyUp(Keys.ARROW_DOWN)
-		.keyDown(Keys.ENTER).keyUp(Keys.ENTER).build().perform();
 
+//		actions.contextClick(womenTab).build().perform();
+
+//		actions.contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+
+		actions.moveToElement(womenTab).contextClick().pause(5000).keyDown(Keys.ARROW_DOWN).keyUp(Keys.ARROW_DOWN)
+				.keyDown(Keys.ENTER).keyUp(Keys.ENTER).build().perform();
+
+	}
+
+	public void scrollToAnElementUsingActions(WebElement element) {
+		Actions actions = new Actions(driver);
+		actions.scrollToElement(element).build().perform();
+	}
+
+	public void launchBrowser(String browserName) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\LENOVO\\git\\SaraswathyJavaProject\\drivers\\chromedriver_109.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	public void launchApplication(String appName, long implicitWaitSeconds) {
+		switch (appName) {
+		case "Crocs":
+			driver.get("https://www.crocs.in/");
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitSeconds));
+		}
 	}
 
 	public void clickAnElementUsingActions(WebElement ele) {
 		Actions actions = new Actions(driver);
 		actions.moveToElement(ele).build().perform();
 	}
-	
+
 	public void pressAKeyUsingActionsClass(String keyName) {
 		Actions actions = new Actions(driver);
-		switch(keyName) {
+		switch (keyName) {
 		case "a":
 //			actions.keyDown(Keys.)
 		}
